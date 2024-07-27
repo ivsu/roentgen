89d3639f3a34dd4702c0d22e0154063afb93cd9b
ivsu <2492817+ivsu@users.noreply.github.com>
2024-07-27 11:28:21 +0300

    change bot's folder in colab

--- container/settings.py
+++ container/settings.py
@@ -3,15 +3,15 @@ import os
 # используемая БД
 DB_VERSION = ['PG', 'SQLite'][1]
 
-PROJECT_FOLDER = '/content/roentgen/container/'
-BOTS_FOLDER = PROJECT_FOLDER + 'bots/'
-DRIVE_FOLDER = '/content/drive/MyDrive/university/roentgen/'  # TODO: исключить использование
+DRIVE_FOLDER = '/content/drive/MyDrive/university/roentgen/'
 LOCAL_FOLDER = '/Users/ivan/Documents/CIFROPRO/Проекты/Нейронки/Расписание рентген-центра/'
 
 try:
     # выполнение в колабе
     import google.colab
     RUN_ENV = 'COLAB'
+    PROJECT_FOLDER = '/content/roentgen/container/'
+    BOTS_FOLDER = DRIVE_FOLDER + 'bots/'
     # if not os.path.exists('/content/drive/'):
     #     from google.colab import drive
     #     drive.mount('/content/drive/')
