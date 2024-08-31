6ecfaf03ead5d05ca1bddc5db48e335b0de01a45
ivsu <2492817+ivsu@users.noreply.github.com>
2024-08-31 13:15:08 +0300

    show_legend

--- container/common/showdata.py
+++ container/common/showdata.py
@@ -105,7 +105,7 @@ def show_doctors():
 
 
 def show_legend():
-    print('Расшифровка модальностей врачей')
+    print('Расшифровка модальностей врачей:')
     print("\n".join(f'{k:>7}: {v}' for k, v in CHANNEL_LEGEND.items()))
 
 
