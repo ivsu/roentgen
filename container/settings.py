d90cc1682f6850d958db34bbe49573078d5af546
ivsu <2492817+ivsu@users.noreply.github.com>
2024-11-17 19:07:23 +0300

    project folders

--- container/settings.py
+++ container/settings.py
@@ -11,14 +11,17 @@ try:
     import google.colab
     RUN_ENV = 'COLAB'
     PROJECT_FOLDER = '/content/roentgen/container/'
-    BOTS_FOLDER = DRIVE_FOLDER + 'bots/'
+    # папка для загрузки данных из Excel
+    XLS_FILEPATH = '/content/project/src_files/'
+    # BOTS_FOLDER = DRIVE_FOLDER + 'bots/'
+    BOTS_FOLDER = '/content/project/bots/'
     # if not os.path.exists('/content/drive/'):
     #     from google.colab import drive
-    #     drive.mount('/content/drive/')
 
-except:
+except ModuleNotFoundError:
     RUN_ENV = 'IDE'
     PROJECT_FOLDER = '/Users/ivan/PycharmProjects/roentgen/container/'
+    XLS_FILEPATH = '/Users/ivan/Documents/CIFROPRO/Проекты/Нейронки/Расписание рентген-центра/dataset/'
     BOTS_FOLDER = LOCAL_FOLDER + 'bots/'
 
 # настройки для подключения к БД (только для PG)
@@ -28,4 +31,7 @@ os.environ['RUN_ENV'] = RUN_ENV
 
 if not os.path.exists(BOTS_FOLDER):
     os.mkdir(BOTS_FOLDER)
+
+if __name__ == '__main__':
+    print('RUN_ENV:', RUN_ENV)
 
